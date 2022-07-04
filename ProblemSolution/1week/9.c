d13960e1a5c0d08f1ae4b36b898bfd5541b541b3
woong <hyunwoong8280@gmail.com>
2022-07-04 16:45:36 +0900

    [~1week]

--- ProblemSolution/1week/9.c
+++ ProblemSolution/1week/9.c
@@ -3,15 +3,15 @@
 int main (){
     
     //9 : 파이값 근사적으로 계산하기
-    int pinum;
+    int pi;
     int devideNum;
     int signNum = 1;
     double piInitNum = 4;
     double piSum = 0;
     printf("얼마만큼 근사적으로 파이값을 구할것인가요? :");
-    scanf("%d" , &pinum);
+    scanf("%d" , &pi);
 
-    for(int i=0; i<pinum; i++){
+    for(int i=0; i<pi; i++){
         devideNum = (2 * i + 1) * signNum;
         piSum += 4/(double)devideNum;
         signNum *= -1;
