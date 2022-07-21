938f221ac367b12f0b9b7b3316b7c3fb35c13709
woong <dnddl8280@naver.com>
2022-07-21 23:32:16 +0900

    [solved]

---
+++ ProblemSolution/8week/6.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#define MAX 100
+int main(){
+    int N;
+    int arr[MAX][MAX];
+
+    FILE * file = fopen("input6.txt" , "r");
+    fscanf(file , "%d" , &N);
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++) fscanf(file , "%d" , &arr[i][j]);
+    }
+    fclose(file);
+
+    int printCnt = 0;
+    int printDir = 0;
+    int startX = 0 , limitX = N - 1 ;
+    int startY = 0 , limitY = N - 1;
+    // printDir 
+    // 0 right
+    // 1 down
+    // 2 left
+    // 3 up
+    
+    while(printCnt < N * N){
+        if(printDir % 4 == 0){
+            for(int i = startY; i <= limitY; i ++){
+                printf("%d ", arr[startX][i]);
+                printCnt ++;
+            }
+            printDir = 1;
+            startX ++;
+        }else if(printDir % 4 == 1){
+            for(int i = startX; i <= limitX; i ++){
+                printf("%d ", arr[i][limitY]);
+                printCnt ++;
+            }
+            printDir = 2;
+            limitY --;
+        }else if(printDir % 4 == 2){
+            for(int i = limitY; i >= startY; i --){
+                printf("%d ", arr[limitX][i]);
+                printCnt ++;
+            }
+            printDir = 3;
+            limitX --;
+        }else  if(printDir % 4 == 3){
+            for(int i = limitX; i >= startX; i --){
+                printf("%d " , arr[i][startY]);
+                printCnt++;
+            }
+            printDir = 4;
+            startY++;
+        }
+    }
+
+    return 0;
+}
