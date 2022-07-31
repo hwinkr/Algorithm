5027dbbdf860a0c0d7fd68f45713bf87b1ef0dac
woong <dnddl8280@naver.com>
2022-07-31 22:28:39 +0900

    [같은수 만들기]

---
+++ Python/baekjoon/greedy/16953.py
@@ -0,0 +1,18 @@
+a, b = map(int, input().split())
+count = 0
+
+while True:
+    if(b % 10 == 1):
+        count += 1
+        b //= 10
+    else:
+        count += 1
+        b /= 2
+
+    if(b <= a):
+        break
+
+if(b == a):
+    print(count + 1)
+else:
+    print(-1)
