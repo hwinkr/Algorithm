294538aead067e87b8b4544d210592c578ec41c5
woong <dnddl8280@naver.com>
2022-08-02 23:13:02 +0900

    [py algorithm study]

--- Python/Study/Search/1.py
+++ Python/Study/Search/1.py
@@ -1,35 +1,35 @@
 # 1
-# N = int(input())
+N = int(input())
 
-# direction = input().split()
+direction = input().split()
 
-# x = 1
-# y = 1
+x = 1
+y = 1
 
-# dx = [-1, 0, 1, 0]
-# dy = [0, 1, 0, -1]
-# # 공간을 벗어나는 이동은 무시
-# for dir in direction:
-#     x_index = 0
-#     y_index = 0
-#     if(dir == "U"):
-#         x_index = dx[0]
-#         y_index = dy[0]
-#     elif(dir == "R"):
-#         x_index = dx[1]
-#         y_index = dy[1]
-#     elif(dir == "D"):
-#         x_index = dx[2]
-#         y_index = dy[2]
-#     else:
-#         x_index = dx[3]
-#         y_index = dy[3]
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+# 공간을 벗어나는 이동은 무시
+for dir in direction:
+    x_index = 0
+    y_index = 0
+    if(dir == "U"):
+        x_index = dx[0]
+        y_index = dy[0]
+    elif(dir == "R"):
+        x_index = dx[1]
+        y_index = dy[1]
+    elif(dir == "D"):
+        x_index = dx[2]
+        y_index = dy[2]
+    else:
+        x_index = dx[3]
+        y_index = dy[3]
 
-#     if(x + x_index > 0 and x + x_index < N and y + y_index > 0 and y + y_index < N):
-#         x += x_index
-#         y += y_index
+    if(x + x_index > 0 and x + x_index < N and y + y_index > 0 and y + y_index < N):
+        x += x_index
+        y += y_index
 
-# print(x, y)
+print(x, y)
 
 # 2
 N = int(input())
