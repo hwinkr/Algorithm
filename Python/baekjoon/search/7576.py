a97170b3741216e054fbb73d37f554076f337e25
woong <dnddl8280@naver.com>
2022-08-07 10:41:51 +0900

    [토마토]

--- Python/baekjoon/search/7576.py
+++ Python/baekjoon/search/7576.py
@@ -5,64 +5,61 @@
 from collections import deque
 import sys
 
-# m, n = map(int, sys.stdin.readline().split())
-# graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+m, n = map(int, sys.stdin.readline().split())
+graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 
 
-# def bfs_my(que):
-#     day = 0
-#     size = len(que)
-#     cnt = 0
-#     while que:
-#         x, y = que.popleft()
+def bfs_my(que):
+    day = 0
+    size = len(que)
+    cnt = 0
+    while que:
+        x, y = que.popleft()
 
-#         for i in range(4):
-#             nx = x + dx[i]
-#             ny = y + dy[i]
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-#             if((0 <= nx < n) and (0 <= ny < m)):
-#                 if(graph[nx][ny] == 0):
-#                     graph[nx][ny] = 1
-#                     que.append((nx, ny))
-#         cnt += 1
+            if((0 <= nx < n) and (0 <= ny < m)):
+                if(graph[nx][ny] == 0):
+                    graph[nx][ny] = 1
+                    que.append((nx, ny))
+        cnt += 1
 
-#         if(cnt == size):
-#             day += 1
-#             size += len(que)
+        if(cnt == size):
+            day += 1
+            size += len(que)
 
-#     return day - 1
+    return day - 1
 
 
-# dx = [-1, 1, 0, 0]
-# dy = [0, 0, -1, 1]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-# day_que = deque()
+day_que = deque()
 
-# for i in range(n):
-#     for j in range(m):
-#         if(graph[i][j] == 1):
-#             day_que.append((i, j))
+for i in range(n):
+    for j in range(m):
+        if(graph[i][j] == 1):
+            day_que.append((i, j))
 
-# result = bfs_my(day_que)
-# success = 1
+success = 1
 
-# for i in range(n):
-#     for j in range(m):
-#         if(graph[i][j] == 0):
-#             success = 0
-#             break
-# if(success):
-#     print(result)
-# else:
-#     print(-1)
+for i in range(n):
+    for j in range(m):
+        if(graph[i][j] == 0):
+            success = 0
+            break
+
+if(success):
+    print(bfs_my(day_que))
+else:
+    print(-1)
 
 # 좀 더 나은 풀이
 # 상자에 안익은 토마토를 얼마나 가지고 있는지 세고 출발하는 것도 좋은 방법
 # 함수의 반환값에 따라서 출력값이 달라지는 경우에는 그 함수의 반환값을 변수로 사용해서 그 변수를 이용하는것 보다 함수 안에서 모든 출력값을 조절할 수 있도록 설정하면 더 나은 코드와 효율적인 매모리 사용이 가능하다.
 # 내 코드와 비교하면 메모리 사용이 거의 절반임
-
-from collections import deque
-import sys
 
 
 def bfs():
