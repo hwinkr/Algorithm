29ccf9d72d17a26e2df9e3c7c4c21ea916466b79
hwinkr <dnddl8280@naver.com>
2023-03-04 16:23:26 +0900

    2023.03.04 solved

---
+++ Programmers/2023 KAKAO BLIND RECRUITMENT/이모티콘 할인행사.py
@@ -0,0 +1,56 @@
+def sol(users: list, emoticons: list, tmp: list, rates: list) -> list:
+    global total_member, total_money
+    if len(tmp) == len(emoticons):
+        member, money = 0, 0
+
+        for user in users:
+            is_member = False
+            rate, limit = user
+            curr = 0
+            for i in range(len(emoticons)):
+                if rate <= tmp[i]:
+                    curr += emoticons[i] - (emoticons[i] // 100 * tmp[i])
+                    if curr >= limit:
+                        is_member = True
+                        member += 1
+                        break
+
+            if not is_member:
+                money += curr
+
+        if member > total_member:
+            total_member = member
+            total_money = money
+        elif member == total_member and money > total_money:
+            total_money = money
+
+        return
+
+    for i in range(4):
+        tmp.append(rates[i])
+        sol(users, emoticons, tmp, rates)
+        tmp.pop()
+
+
+total_member, total_money = -1, -1
+
+
+def solution(users, emoticons) -> list:
+    sol(users, emoticons, [], [10, 20, 30, 40])
+    return [total_member, total_money]
+
+
+print(
+    solution(
+        [
+            [40, 2900],
+            [23, 10000],
+            [11, 5200],
+            [5, 5900],
+            [40, 3100],
+            [27, 9200],
+            [32, 6900],
+        ],
+        [1300, 1500, 1600, 4900],
+    )
+)
